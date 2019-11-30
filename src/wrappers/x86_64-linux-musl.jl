17679b02c254f676f3315c3e85ea672784795dc0
jlbuild <juliabuildbot@gmail.com>
2019-11-30 22:25:11 +0000

    Xorg_xcb_util_renderutil_jll build 0.3.9+0

--- src/wrappers/x86_64-linux-musl.jl
+++ src/wrappers/x86_64-linux-musl.jl
@@ -25,16 +25,18 @@ const libxcb_render_util = "libxcb-render-util.so.0"
 Open all libraries
 """
 function __init__()
-    global prefix = abspath(joinpath(@__DIR__, ".."))
+    global artifact_dir = abspath(artifact"Xorg_xcb_util_renderutil")
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
     # We first need to add to LIBPATH_list the libraries provided by Julia
-    LIBPATH_list = [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]
-    append!.(Ref(PATH_list), (Xorg_xcb_util_jll.PATH_list,))
-    append!.(Ref(LIBPATH_list), (Xorg_xcb_util_jll.LIBPATH_list,))
+    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
+    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
+    # then append them to our own.
+    foreach(p -> append!(PATH_list, p), (Xorg_xcb_util_jll.PATH_list,))
+    foreach(p -> append!(LIBPATH_list, p), (Xorg_xcb_util_jll.LIBPATH_list,))
 
-    global libxcb_render_util_path = abspath(joinpath(artifact"Xorg_xcb_util_renderutil", libxcb_render_util_splitpath...))
+    global libxcb_render_util_path = normpath(joinpath(artifact_dir, libxcb_render_util_splitpath...))
 
     # Manually `dlopen()` this right now so that future invocations
     # of `ccall` with its `SONAME` will find this path immediately.
